426669899938ec13515f8c63607be3a809f762ef
alexfrst <alex.forest1026@gmail.com>
2021-12-29 12:15:18 +0100

    feat: linting + endpoint for JS code testing

--- quizz.py
+++ quizz.py
@@ -5,35 +5,45 @@ Created on Sat Dec 18 12:11:26 2021
 @author: Morgane
 """
 
-
-#For the API Flask
+# For the API Flask
 from flask import Flask
-from flask import render_template
+import random
+from flask import render_template, jsonify
 from flask import request
-
 
 '''_______________________________________APP FLASK_______________________________________'''
 
-
-app = Flask(__name__,template_folder="templates") 
+app = Flask(__name__, template_folder="templates")
 
 app.config["SECRET_KEY"] = "alexandremorgane"
 
 app = Flask(__name__)
 
 
-#default
+# default
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
 
 @app.route('/index.html')
 def play():
-    return render_template('index.html') 
+    return render_template('index.html')
+
 
 @app.route('/rules.html')
 def rules():
-    return render_template('rules.html') 
+    return render_template('rules.html')
+
+
+@app.route('/newquestion')
+def new_question():
+    random_response = {"question": f"This is random question {random.randint(1, 100)}",
+                       "right_answer": f"right answer",
+                       "wrong_answers": [f"wrong answer {i + 1}" for i in range(3)]},
+
+    return jsonify(random_response)
+
 
 """
 @app.route('/', methods=['POST'])
@@ -46,12 +56,9 @@ def start():
 
 '''_______________________________________QUERIES_______________________________________'''
 
-
 '''_______________________________________MAIN_______________________________________'''
-   
 
 if __name__ == "__main__":
-    
-    #Launch our Website
+    # Launch our Website
     print("\nLaunch :")
     app.run()
