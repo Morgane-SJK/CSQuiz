0756857a9142c4ba5bcd1c59b56501a42acdf23f
alexfrst <alex.forest1026@gmail.com>
2022-02-10 18:31:33 +0100

    refact: moved theme related classes in subfolder

---
+++ question_generator/classes/films.py
@@ -0,0 +1,5 @@
+class Films:
+    def __init__(self):
+        self.properties = [
+            "dbo:director", "dbo:producer", "dbo:starring", "dbo:budget", "dbo:gross"
+        ]
--- question_generator/films.py
+++
@@ -1,5 +0,0 @@
-class Films:
-    def __init__(self):
-        self.properties = [
-            "dbo:director", "dbo:producer", "dbo:starring","dbo:budget", "dbo:gross"
-        ]
