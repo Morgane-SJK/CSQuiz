a6864e2946547f70cd4b6b96e0019fa5a2bdd4e0
alexfrst <alex.forest1026@gmail.com>
2022-02-22 13:09:36 +0100

    feat: Larousse scrapper used to get word's gender

---
+++ question_generator/french_dictionnary_scrapper.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_word_gender(word):
+    response = requests.get(f"https://www.larousse.fr/dictionnaires/francais/{word}")
+    soup = BeautifulSoup(response.content, 'html.parser')  # the parser that suits to the html
+    wanted_words = soup.find_all("a", string=word)
+
+    if len(wanted_words) == 0:
+        genders = soup.select("p.CatgramDefinition")
+        if len(genders) > 0:
+            if genders[0].text == "nom féminin":
+                return "F"
+        return "M"
+    else:
+        word_endpoint = wanted_words[0]["href"]
+        response = requests.get(f"https://www.larousse.fr{word_endpoint}")
+        soup = BeautifulSoup(response.content, 'html.parser')
+        words = soup.select(
+            'div.wrapper div.row div.col-md-8 div#definition article div.header-article p.CatgramDefinition')
+        gender = words[0].text
+
+        if gender == "nom féminin":
+            return "F"
+        else:
+            return "M"
+
+
+if __name__ == '__main__':
+    print(get_word_gender("république"))
+    print(get_word_gender("capitale"))
