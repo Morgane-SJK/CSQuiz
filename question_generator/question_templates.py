6687ef5b2fb5a25ea09a6512617cc3088c63c5fd
alexfrst <alex.forest1026@gmail.com>
2022-02-27 12:34:31 +0100

    feat: upgraded French syntaxing template

--- question_generator/question_templates.py
+++ question_generator/question_templates.py
@@ -9,25 +9,33 @@ class QuestionType(Enum):
 gender_memory = {}
 
 
-def build_question(subject, predicate, object_range, language) -> str:
+def build_question(subject, predicate, object_range, language, subject_range) -> str:
     persons_range = ["http://dbpedia.org/ontology/Agent", "http://dbpedia.org/ontology/Person",
                      "http://dbpedia.org/ontology/Actor", "http://dbpedia.org/ontology/Painter",
                      "http://dbpedia.org/ontology/Athlete", "http://dbpedia.org/ontology/Commander"]
-    if language == 'English':
-        if object_range in persons_range:
-            return f"Who is {predicate} in {subject}?"
-        else:
-            return f"What is the {predicate} of {subject}?"
-    elif language == 'French':
 
+    if language == 'French':
         if object_range in persons_range:
             return f"Qui est {predicate} dans {subject}?"
         else:
             determinant_pred = determinant_handler_predicate_french(predicate)
-            determinant_subj = determinant_handler_subject_french(subject)
+            determinant_subj = determinant_handler_subject_french(subject, subject_range)
+            pronoun = pronoun_handler_french(predicate)
 
-            return f'Quel est {determinant_pred} {predicate} {determinant_subj} {subject}?'
+            return f'{pronoun} est {determinant_pred} {predicate} {determinant_subj} {subject}?'
 
+    if object_range in persons_range:
+        return f"Who is {predicate} in {subject}?"
+    else:
+        return f"What is the {predicate.replace('active years','')} of {subject}?"
+
+def pronoun_handler_french(predicate):
+    if predicate not in gender_memory:
+        gender_memory[predicate] = get_word_gender(predicate.lower())
+
+    if gender_memory[predicate] == "F":
+        return "Quelle"
+    return "Quel"
 
 def determinant_handler_predicate_french(predicate):
     vowels = "aeiou"
@@ -44,7 +52,7 @@ def determinant_handler_predicate_french(predicate):
     return "le"
 
 
-def determinant_handler_subject_french(subject):
+def determinant_handler_subject_french(subject, subject_range):
     vowels = "aeiou"
     if subject[0].lower() in vowels:
         return "d'"
@@ -53,6 +61,10 @@ def determinant_handler_subject_french(subject):
 
     if first_word[-1] == "s":
         return "des"
+
+    print(subject_range)
+    if subject_range in ["http://dbpedia.org/ontology/Work"]:
+        return "de"
 
     if first_word not in gender_memory:
         gender_memory[first_word] = get_word_gender(first_word)
