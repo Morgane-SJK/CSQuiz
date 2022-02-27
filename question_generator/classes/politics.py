7b22b8634aa344205bbd9b0be46bb1d62cb65676
alexfrst <alex.forest1026@gmail.com>
2022-02-27 12:31:41 +0100

    feat: Theme superclass implementation

--- question_generator/classes/politics.py
+++ question_generator/classes/politics.py
@@ -1,5 +1,13 @@
-class Politics:
+from question_generator.classes.theme import Theme
+
+
+class Politics(Theme):
     def __init__(self):
+        super().__init__()
         self.properties = [
-            "dbo:party"
+            "dbo:party",
+            "dbo:europeanParliamentGroup",
+            "dbo:politicalPartyInLegislature",
+            "dbo:politicalPartyOfLeader",
         ]
+        self.wiki_page_length = 10000
