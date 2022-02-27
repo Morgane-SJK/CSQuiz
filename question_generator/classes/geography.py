333f6a89cbf55f63c88f988a7a02ae8a31bc1635
alexfrst <alex.forest1026@gmail.com>
2022-02-27 14:13:07 +0100

    fix: Predicates format and min page length

--- question_generator/classes/geography.py
+++ question_generator/classes/geography.py
@@ -4,7 +4,5 @@ from question_generator.classes.theme import Theme
 class Geography(Theme):
     def __init__(self):
         super().__init__()
-        self.properties = [
-            "dbo:capital", "dbo:currency", "dbo:humanDevelopmentIndex"
-        ],
-        self.wiki_page_length = 400000
+        self.properties = ["dbo:capital", "dbo:currency", "dbo:humanDevelopmentIndex"]
+        self.wiki_page_length = 300000
